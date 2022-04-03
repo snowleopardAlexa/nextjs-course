9a83a653d5f523c464391ddad5752ec377e2e57c
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-03 16:13:14 -0400

    add fs and path

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+// file system - can be ran on the server side 
+import path from 'path'
+import fs from 'fs/promises'
+
 function Home(props) {
 
   const { products } = props
@@ -13,9 +17,15 @@ function Home(props) {
 
 export default Home
 
-// this code doesn't run on the client side
+// this code doesn't run on the client side - this runs on the server side
 // pre-fetch data before this component get pre-render by next.js
+// the code is now executed on the SERVER-SIDE 
+// cwd - current working directory
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const jsonData = await fs.readFile(filePath)
+  const data = JSON.parse(javascript)
+
   return { 
     props: {
       products: [{id: 'p1', title: 'Product 1'}]
